61d085d0094b68bb0af8b842cedf17437ebeac7a
kanwar-1990 <knwrpl.singh@gmail.com>
2021-07-22 15:56:06 +0530

    adding addclass.java file

---
+++ src/main/java/ImpSeleniumSession/addclass.java
@@ -0,0 +1,10 @@
+package ImpSeleniumSession;
+
+public class addclass {
+	
+	public void ToTestIndividualmethod()
+	{
+		System.out.println("Checking whether individual wrapper class adding in Git or not");
+	}
+
+}
