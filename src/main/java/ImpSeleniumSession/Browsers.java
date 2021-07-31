c65591a1cca560fdd88b1e25a830f67f553d9137
kanwar-1990 <knwrpl.singh@gmail.com>
2021-07-31 14:44:03 +0530

    switch case , practive class

---
+++ src/main/java/ImpSeleniumSession/Browsers.java
@@ -0,0 +1,39 @@
+package ImpSeleniumSession;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Browsers {
+	public static void main(String[] args) {
+
+		WebDriver driver = null;
+		String browser = "firefox";
+		switch (browser) {
+		case "chrome":
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.chromedriver().setup();
+
+			driver = new FirefoxDriver();
+			break;
+
+		case "Safari":
+			WebDriverManager.chromedriver().setup();
+			driver = new SafariDriver();
+			break;
+
+		default:
+			System.out.println("Please type the correct browser Name");
+			break;
+		}
+
+		driver.get("https://www.google.com/");
+	}
+}
---
+++ src/main/java/ImpSeleniumSession/practive.java
@@ -0,0 +1,44 @@
+package ImpSeleniumSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class practive {
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.amazon.com/");
+		ArrayList<String> PrintEle = getAppleProductListAmazon(driver, "Apple");
+
+		System.out.println(PrintEle);
+	}
+
+	public static ArrayList<String> getAppleProductListAmazon(WebDriver driver, String ProductName) {
+		driver.findElement(By.cssSelector("input[type='text']")).sendKeys(ProductName);
+		driver.findElement(By.cssSelector("#nav-search-submit-button")).click();
+
+		List<WebElement> AppleListItem = driver
+				.findElements(By.cssSelector(".a-size-medium.a-color-base.a-text-normal"));
+
+		System.out.println("The total number of the products are : " + AppleListItem.size());
+
+		ArrayList<String> ListitOut = new ArrayList<String>();
+
+		for (WebElement webElement : AppleListItem) {
+			String ListofItems = webElement.getText();
+
+			ListitOut.add(ListofItems);
+		}
+		System.out.println("List of the Items are");
+		return ListitOut;
+	}
+
+}
