c2d686759c1dda95561afb501de45b4397da8c94
kanwar-1990 <knwrpl.singh@gmail.com>
2021-08-21 16:08:40 +0530

    Adding Signup Page to signupbranch

---
+++ src/main/java/ImpSeleniumSession/SigupPage.java
@@ -0,0 +1,11 @@
+package ImpSeleniumSession;
+
+public class SigupPage {
+	
+	
+	public SigupPage() {
+		// TODO Auto-generated constructor stub
+		System.out.println("I am the SignUpPage");
+	}
+
+}
